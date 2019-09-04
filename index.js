b82eca6c2cf01ed6671d9d26c6a0ec9f24362355
Alexander Kidd <alexanderkidd5@outlook.com>
2019-09-03 23:38:02 -0400

    Forget ajax.

--- index.js
+++ index.js
@@ -235,44 +235,45 @@ function startFactCheck() {
   var urlToCheck = document.getElementById('url_box').value;
 
   if(urlToCheck != url) {
-    $.ajax({
-      type: "GET",
-      url: urlToCheck,
-      dataType: "html",
-      headers: {
-        'Access-Control-Allow-Headers': 'x-requested-with, Content-Type, Authorization, Origin, Accept',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'X-Requested-With': 'XMLHttpRequest'
-      },
-      async: true,
-      success: function (html) {
-        $("#loadedPage").loaded(urlToCheck, function() {
-          var contentParse = contentScrape(html, this.url);
+    $("#loadedPage").loaded(urlToCheck, function() {
+      var contentParse = contentScrape(html, this.url);
 
-          // Immediately post relevant data to background.js
-          bgWorker.postMessage({"contentParse" : contentParse});
+      // Immediately post relevant data to background.js
+      bgWorker.postMessage({"contentParse" : contentParse});
 
-          // Query the background script for factoid data.  This should probably be a listener of sorts.
-          pollFactData();
-          clearInterval(pollInterval);
-          pollInterval = setInterval(pollFactData, 500);
+      // Query the background script for factoid data.  This should probably be a listener of sorts.
+      pollFactData();
+      clearInterval(pollInterval);
+      pollInterval = setInterval(pollFactData, 500);
 
-          // Continuously build/update the UI as factoid data is processed.
-          buildUI();
-          clearInterval(buildUIInterval);
-          buildUIInterval = setInterval(buildUI, 250);
-        });
-
-      },
-      error: function (xhr, status, error) {
-        console.error("Error: startFactCheck() could not get requested page to check. Message: " + error +
-        "." + "\n" + "Site: " + url);
-
-        document.getElementById("fact_text").style.color="#FF0000";
-        document.getElementById("fact_text").innerHTML = "404 Page Not Found.<br><br>URL Invalid or Site Is Offline.";
-      }
+      // Continuously build/update the UI as factoid data is processed.
+      buildUI();
+      clearInterval(buildUIInterval);
+      buildUIInterval = setInterval(buildUI, 250);
     });
+    // $.ajax({
+    //   type: "GET",
+    //   url: urlToCheck,
+    //   dataType: "html",
+    //   headers: {
+    //     'Access-Control-Allow-Headers': 'x-requested-with, Content-Type, Authorization, Origin, Accept',
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Methods': 'GET',
+    //     'X-Requested-With': 'XMLHttpRequest'
+    //   },
+    //   async: true,
+    //   success: function (html) {
+    //
+    //
+    //   },
+    //   error: function (xhr, status, error) {
+    //     console.error("Error: startFactCheck() could not get requested page to check. Message: " + error +
+    //     "." + "\n" + "Site: " + url);
+    //
+    //     document.getElementById("fact_text").style.color="#FF0000";
+    //     document.getElementById("fact_text").innerHTML = "404 Page Not Found.<br><br>URL Invalid or Site Is Offline.";
+    //   }
+    // });
   }
 }
 
